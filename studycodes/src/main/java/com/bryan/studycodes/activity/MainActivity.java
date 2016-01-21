abe4fce776d03e30c0ec89957f702d372ce00498
cxbiao <690158801@qq.com>
2016-01-21 13:37:38 +0800

    fix

--- studycodes/src/main/java/com/bryan/studycodes/activity/MainActivity.java
+++ studycodes/src/main/java/com/bryan/studycodes/activity/MainActivity.java
@@ -50,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
