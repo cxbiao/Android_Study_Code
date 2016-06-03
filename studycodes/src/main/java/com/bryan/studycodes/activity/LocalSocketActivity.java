0d995ab983a0f9383be619537582871e7b09183f
cxbiao <690158801@qq.com>
2016-06-03 11:29:00 +0800

    update

--- studycodes/src/main/java/com/bryan/studycodes/activity/LocalSocketActivity.java
+++ studycodes/src/main/java/com/bryan/studycodes/activity/LocalSocketActivity.java
@@ -1,5 +1,6 @@
 package com.bryan.studycodes.activity;
 
+import android.net.Credentials;
 import android.net.LocalServerSocket;
 import android.net.LocalSocket;
 import android.net.LocalSocketAddress;
@@ -15,19 +16,20 @@ import java.io.OutputStream;
 
 /**
  * Author：Cxb on 2016/6/2 11:42
+ *
+ * MediaRecorder的捕获流可以输出到localsocket
  */
 public class LocalSocketActivity extends TitleBaseActivity {
 
     private static final String TAG = "LocalSocketActivity";
-    private LocalSocket receiver;
-    private LocalSocket sender;
+    private static final String SOCKET_NAME="H264";
+    private static final int BUFFER_SIZE = 500000;
+    private LocalSocket client;
+    private LocalSocket server;
     private LocalServerSocket lss;
-
-    private int BUFFER_SIZE = 500000;
 
     private boolean running ;
 
-    private int i;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,73 +41,94 @@ public class LocalSocketActivity extends TitleBaseActivity {
     private void initSocket() {
 
         try {
-            receiver = new LocalSocket();
-            lss = new LocalServerSocket("H264");
+            client = new LocalSocket();
+            lss = new LocalServerSocket(SOCKET_NAME);
 
             //LocalServerSocket和LocalSocketAddress的name要相同
-            receiver.connect(new LocalSocketAddress("H264"));
-            receiver.setReceiveBufferSize(BUFFER_SIZE);
-            receiver.setSendBufferSize(BUFFER_SIZE);
+            client.connect(new LocalSocketAddress(SOCKET_NAME));
+            client.setReceiveBufferSize(BUFFER_SIZE);
+            client.setSendBufferSize(BUFFER_SIZE);
 
-            sender = lss.accept();
-            sender.setReceiveBufferSize(BUFFER_SIZE);
-            sender.setSendBufferSize(BUFFER_SIZE);
+            server = lss.accept();
+            Credentials cre= server.getPeerCredentials();
+            Log.e(TAG,cre.getUid()+"");
+            server.setReceiveBufferSize(BUFFER_SIZE);
+            server.setSendBufferSize(BUFFER_SIZE);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
 
     }
 
-    public void send(View v) {
 
-        if(running) return;
-        running=true;
-        //发送线程
+    public void startServer(View v) {
+        v.setEnabled(false);
         new Thread(new Runnable() {
             @Override
             public void run() {
                 OutputStream os;
+                InputStream is;
+                int i=0;
                 try {
-                    os = sender.getOutputStream();
-                    while (running) {
-                        byte[] data = ("LocalSocket:" + i).getBytes();
-                        os.write(data);
-                        Log.e(TAG, "sender:size--" + data.length + " --- " + new String(data) + " ---");
+                    is = server.getInputStream();
+                    os = server.getOutputStream();
+                    int len;
+                    byte[] data;
+                    while (true) {
+                        data = new byte[1024];
+                        len=is.read(data);
+                        Log.e(TAG, "server:receive-size--" + len + " --- " + new String(data,0,len) + " ---");
+
+                        byte[] writeBytes = ("Hello Client:" + i).getBytes();
+                        os.write(writeBytes);
+                        Log.e(TAG, "server:send-size--" + writeBytes.length + " --- " + new String(writeBytes) + " ---");
                         os.flush();
-                        Thread.sleep(500);
                         i++;
                     }
 
-                  //  os.close();
-                  //  sender.close();
+                    //不能关闭流，流关闭则套接字也关闭
+                    //  os.close();
+                    //  server.close();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         }).start();
 
+    }
+    public void clientSend(View v) {
 
-        //接收线程
+        if(running) return;
+        running=true;
+
         new Thread(new Runnable() {
             @Override
             public void run() {
                 InputStream is;
+                OutputStream os;
+                int i=0;
                 try {
-                    is = receiver.getInputStream();
-
+                    is = client.getInputStream();
+                    os= client.getOutputStream();
                     byte[] data;
                     int len;
 
                     while (running) {
+                        byte[] writeBytes=("LocalSocket:" + i).getBytes();
+                        os.write(writeBytes);
+                        Log.e(TAG, "client:send-size--" + writeBytes.length + " --- " + new String(writeBytes) + " ---");
+                        os.flush();
+
+                        i++;
                         data = new byte[1024];
                         len=is.read(data);
-                        Log.e(TAG, "receiver:size--" + len + " --- " + new String(data,0,len) + " ---");
-                        Thread.sleep(500);
+                        Log.e(TAG, "client:receive-size--" + len + " --- " + new String(data,0,len) + " ---");
+                        Thread.sleep(1000);
 
                     }
 
                    // is.close();
-                   // receiver.close();
+                   // client.close();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -113,7 +136,7 @@ public class LocalSocketActivity extends TitleBaseActivity {
         }).start();
     }
 
-    public void stop(View v) {
+    public void stopClient(View v) {
         running = false;
 
     }
